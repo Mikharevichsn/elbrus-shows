f02cd60fb3ef0c2c67b957418f2566085b628ab5
Mikhail  Borisov <asinistr51@icloud.com>
2020-08-14 15:20:18 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'; 
+import React, {useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Content from './components/Content/Content';
 import { Container, Navbar } from 'reactstrap';
 import NavBar from './components/Navbar/Navbar';
-import './public/app.css'
+import FilmList from './components/Films/FilmList'
+import './public/app.css';
 
 
 function App() {
- 
   return (
-<>
-    <NavBar/>
-   <Container >
- 
- <Content/>
-   </Container>
-   </>
+    <>
+      <NavBar />
+      <Container>
+        <Content />
+        <FilmList/>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Films/FilmList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FilmCard from './FilmCard'
+
+export default function FilmList() {
+  return (
+    <div>
+      <FilmCard/>
+    </div>
+  )
+}
