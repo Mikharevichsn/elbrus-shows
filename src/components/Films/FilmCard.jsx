dddda9c2db03b129aadf53371b193a432835f9e7
Mikhail  Borisov <asinistr51@icloud.com>
2020-08-17 10:30:26 +0300

    ...

--- src/components/Films/FilmCard.jsx
+++ src/components/Films/FilmCard.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody,
+} from 'reactstrap';
 
 export default function FilmCard(props) {
   const { film } = props;
+  console.log(film);
+
   return (
-    <>
-      <img style={{width: '25%' }} src={`${film.posterUrl}`} />
-      {console.log(film)}
-    </>
+    <CardGroup>
+      <Card>
+        <CardImg
+          style={{ width: '25%' }}
+          src={`${film.posterUrl}`}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardTitle>{`"${film.nameRu}", ${film.year}`}</CardTitle>
+          {film.countries.map((el, i) => {
+            if (film.countries.length - 1 === i) {
+              return <CardSubtitle>{el.country}</CardSubtitle>;
+            } else {
+              return <CardSubtitle>{`${el.country}, `}</CardSubtitle>;
+            }
+          })}
+          <CardText>
+            
+          </CardText>
+          <Button>Добавить</Button>
+        </CardBody>
+      </Card>
+    </CardGroup>
   );
 }
