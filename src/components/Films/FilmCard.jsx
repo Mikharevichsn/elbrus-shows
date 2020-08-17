096087fda88dec6a914771a02fb9ca58bc1b1fca
Станислав Веселов <61013428+BearablePain@users.noreply.github.com>
2020-08-17 09:23:37 +0300

    More Detalis release 2

--- src/components/Films/FilmCard.jsx
+++ src/components/Films/FilmCard.jsx
@@ -5,7 +5,7 @@ export default function FilmCard(props) {
   const { film, id } = props;
   return (
     <>
-      <Link   to={`/film/${film.nameEn}`}><img style={{width: '25%' }} src={`${film.posterUrl}`} />
+      <Link to={`/film/${film.nameRu}`}><img style={{width: '25%' }} src={`${film.posterUrl}`} />
       </Link>
     </>
   );
