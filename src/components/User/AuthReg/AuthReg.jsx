20e6a2f0c197e08ac8a2a16a3d000137f5d34c3f
Sergei Mikharevich <mikharevichsn@mail.ru>
2020-08-20 10:59:08 +0300

    новая регистрация v.2

--- src/components/User/AuthReg/AuthReg.jsx
+++ src/components/User/AuthReg/AuthReg.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.sass';
+import UserRegister from '../UserRegister';
+import Login from '../../Login/Login';
 
 export default function AuthReg() {
   return (
@@ -29,78 +31,8 @@ export default function AuthReg() {
         </div>
 
         <div class="user_options-forms" id="user_options-forms">
-          <div class="user_forms-login">
-            <h2 class="forms_title">Авторизация</h2>
-            <form class="forms_form">
-              <fieldset class="forms_fieldset">
-                <div class="forms_field">
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    class="forms_field-input"
-                    required
-                    autofocus
-                  />
-                </div>
-                <div class="forms_field">
-                  <input
-                    type="password"
-                    placeholder="Пароль"
-                    class="forms_field-input"
-                    required
-                  />
-                </div>
-              </fieldset>
-              <div class="forms_buttons">
-                <button type="button" class="forms_buttons-forgot">
-                  {/* Forgot password? */}
-                </button>
-                <input
-                  type="submit"
-                  value="Войти"
-                  class="forms_buttons-action"
-                />
-              </div>
-            </form>
-          </div>
-          <div class="user_forms-signup">
-            <h2 class="forms_title">Регистрация</h2>
-            <form class="forms_form">
-              <fieldset class="forms_fieldset">
-                <div class="forms_field">
-                  <input
-                    type="text"
-                    placeholder="Ваше имя"
-                    class="forms_field-input"
-                    required
-                  />
-                </div>
-                <div class="forms_field">
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    class="forms_field-input"
-                    required
-                  />
-                </div>
-                <div class="forms_field">
-                  <input
-                    type="password"
-                    placeholder="Пароль"
-                    class="forms_field-input"
-                    required
-                  />
-                </div>
-              </fieldset>
-              <div class="forms_buttons">
-                <input
-                  type="submit"
-                  value="Зарегаться"
-                  class="forms_buttons-action"
-                />
-              </div>
-            </form>
-          </div>
+          <Login />
+          <UserRegister />
         </div>
       </div>
     </section>
