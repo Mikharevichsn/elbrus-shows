96a0162597b3d3f100380aa1e432c2d289ec8282
Sergei Mikharevich <mikharevichsn@mail.ru>
2020-08-20 18:48:18 +0300

    fix part 2

--- src/components/Films/FilmCard.jsx
+++ src/components/Films/FilmCard.jsx
@@ -74,12 +74,12 @@ export default function FilmCard(props) {
                     width="1em"
                     height="1em"
                     viewBox="0 0 16 16"
-                    class="bi bi-list"
+                    className="bi bi-list"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                     />
                   </svg>
@@ -104,12 +104,12 @@ export default function FilmCard(props) {
                     width="1em"
                     height="1em"
                     viewBox="0 0 16 16"
-                    class="bi bi-bookmark-fill"
+                    className="bi bi-bookmark-fill"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M3 3a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12l-5-3-5 3V3z"
                     />
                   </svg>
@@ -118,16 +118,16 @@ export default function FilmCard(props) {
                     width="1em"
                     height="1em"
                     viewBox="0 0 16 16"
-                    class="bi bi-bookmark-plus"
+                    className="bi bi-bookmark-plus"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M4.5 2a.5.5 0 0 0-.5.5v11.066l4-2.667 4 2.667V8.5a.5.5 0 0 1 1 0v6.934l-5-3.333-5 3.333V2.5A1.5 1.5 0 0 1 4.5 1h4a.5.5 0 0 1 0 1h-4zm9-1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H13V1.5a.5.5 0 0 1 .5-.5z"
                     />
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M13 3.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0v-2z"
                     />
                   </svg>
@@ -152,12 +152,12 @@ export default function FilmCard(props) {
                     width="1em"
                     height="1em"
                     viewBox="0 0 16 16"
-                    class="bi bi-heart-fill"
+                    className="bi bi-heart-fill"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                     />
                   </svg>
@@ -166,12 +166,12 @@ export default function FilmCard(props) {
                     width="1em"
                     height="1em"
                     viewBox="0 0 16 16"
-                    class="bi bi-heart"
+                    className="bi bi-heart"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
                     />
                   </svg>
--- src/components/User/AuthReg/AuthReg.jsx
+++ src/components/User/AuthReg/AuthReg.jsx
@@ -5,32 +5,32 @@ import Login from '../../Login/Login';
 
 export default function AuthReg() {
   return (
-    <section class="user">
-      <div class="user_options-container">
-        <div class="user_options-text">
-          <div class="user_options-unregistered">
-            <h2 class="user_unregistered-title">Еще нет аккаунта?</h2>
-            <p class="user_unregistered-text">
+    <section className="user">
+      <div className="user_options-container">
+        <div className="user_options-text">
+          <div className="user_options-unregistered">
+            <h2 className="user_unregistered-title">Еще нет аккаунта?</h2>
+            <p className="user_unregistered-text">
               Зарегистрируйся и сохраняй в закладки кинчики, которые планируешь
               посмотреть ;-)
             </p>
-            <button class="user_unregistered-signup" id="signup-button">
+            <button className="user_unregistered-signup" id="signup-button">
               Зарегистрироваться
             </button>
           </div>
 
-          <div class="user_options-registered">
-            <h2 class="user_registered-title">Уже есть аккаунт?</h2>
-            <p class="user_registered-text">
+          <div className="user_options-registered">
+            <h2 className="user_registered-title">Уже есть аккаунт?</h2>
+            <p className="user_registered-text">
               Круто! Авторизуйся и будет тебе Щасье!
             </p>
-            <button class="user_registered-login" id="login-button">
+            <button className="user_registered-login" id="login-button">
               Войти
             </button>
           </div>
         </div>
 
-        <div class="user_options-forms" id="user_options-forms">
+        <div className="user_options-forms" id="user_options-forms">
           <Login />
           <UserRegister />
         </div>
